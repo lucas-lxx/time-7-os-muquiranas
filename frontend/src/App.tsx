ec1eb97b08159ac3bf457c4bf8506448e0dddab8
Yuri_Oliveira <oliveiracc@alu.ufc.br>
2025-06-01 12:33:38 -0300

    primeiro commit Frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <div className='bg-sky-600 text-3xl font-bold underline'>
+        Esqueleto do projeto
+      </div>
+      <div>oi</div>
+    </>
+  );
+}
+
+export default App;
